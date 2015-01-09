2fef5f0d44559df0ddd82f77082e7772022a1591
Noah Hoffman <nhoffman@positron.local>
2015-01-08 23:01:14 -0800

    move venv creation into a function

--- hoist.py
+++ hoist.py
@@ -14,13 +14,16 @@ from os import path
 from urllib2 import urlopen
 from distutils.version import LooseVersion
 
+VENV_URL = 'https://pypi.python.org/packages/source/v/virtualenv'
+VENV_VERSION = '1.11.6'
 
-def fetch(url, dest_dir=''):
-    if dest_dir:
-        try:
-            os.makedirs(dest_dir)
-        except OSError:
-            pass
+
+def fetch(url, dest_dir='.'):
+
+    try:
+        os.makedirs(dest_dir)
+    except OSError:
+        pass
 
     fname = path.join(dest_dir, path.basename(url))
     if not path.exists(fname):
@@ -30,6 +33,33 @@ def fetch(url, dest_dir=''):
         handle.close()
 
     return fname
+
+
+def create_virtualenv(venv, version=VENV_VERSION, base_url=VENV_URL, srcdir='.'):
+
+    venv_tgz = 'virtualenv-{}.tar.gz'.format(version)
+
+    if path.exists(path.join(venv, 'bin', 'activate')):
+        print('virtualenv {} already exists'.format(venv))
+    else:
+        try:
+            import virtualenv
+            if True:
+            # if not LooseVersion(virtualenv.__version__) < LooseVersion(version):
+                raise ImportError
+            print('using system version of virtualenv')
+        except ImportError:
+            print('downloading and extracting virtualenv source to {}'.format(srcdir))
+            archive = fetch(path.join(base_url, venv_tgz), dest_dir=srcdir)
+            with tarfile.open(archive, 'r') as tfile:
+                tfile.extractall(srcdir)
+
+            virtualenv = imp.load_source(
+                'virtualenv',
+                path.join(archive.replace('.tar.gz', ''), 'virtualenv.py'))
+
+        print('creating virtualenv {}'.format(venv))
+        virtualenv.create_environment(venv)
 
 
 def main(arguments):
@@ -45,28 +75,9 @@ def main(arguments):
     args = parser.parse_args(arguments)
 
     py_version = "{}.{}.{}".format(*sys.version_info[:3])
-    venv_url = 'https://pypi.python.org/packages/source/v/virtualenv'
-    venv_version = '1.11.6'
-    venv_tgz = 'virtualenv-{}.tar.gz'.format(venv_version)
 
     venv = args.venv
-
-    if not path.exists(path.join(venv, 'bin', 'activate')):
-        try:
-            import virtualenv
-            if not LooseVersion(virtualenv.__version__) < LooseVersion(venv_version):
-                raise ImportError
-        except ImportError:
-            archive = fetch(path.join(venv_url, venv_tgz), dest_dir=args.src)
-
-            with tarfile.open(archive, 'r') as tfile:
-                tfile.extractall(args.src)
-
-            virtualenv = imp.load_source(
-                'virtualenv',
-                path.join(archive.replace('.tar.gz', ''), 'virtualenv.py'))
-
-        virtualenv.create_environment(venv)
+    create_virtualenv(args.venv, srcdir=args.src)
 
 
 if __name__ == '__main__':
