89360527623047ad05c60df94bbfccbcd40add91
Noah Hoffman <nhoffman@positron.local>
2015-01-09 22:07:28 -0800

    add wheel subcommand

--- hoist.py
+++ hoist.py
@@ -10,6 +10,7 @@ import argparse
 import imp
 import tarfile
 import textwrap
+import subprocess
 
 from os import path
 from urllib2 import urlopen
@@ -18,14 +19,25 @@ from distutils.version import LooseVersion
 VENV_URL = 'https://pypi.python.org/packages/source/v/virtualenv'
 VENV_VERSION = '1.11.6'
 
+PY_VERSION = "{}.{}.{}".format(*sys.version_info[:3])
+WHEELSTREET_SYSTEM = os.environ.get('WHEELSTREET', '/usr/local/share/python/wheels')
+WHEELSTREET_USER = os.environ.get('WHEELSTREET', '~/wheels')
 
-def fetch(url, dest_dir='.'):
+WHEEL_PKG = 'wheel==0.24.0'
 
+
+def mkdir(pth):
     try:
-        os.makedirs(dest_dir)
+        os.makedirs(pth)
     except OSError:
         pass
 
+    return path.abspath(pth)
+
+
+def fetch(url, dest_dir='.'):
+
+    mkdir(dest_dir)
     fname = path.join(dest_dir, path.basename(url))
     if not path.exists(fname):
         handle = urlopen(url)
@@ -73,7 +85,7 @@ class Subparser(object):
         self.add_arguments()
 
 
-class Create(Subparser):
+class Virtualenv(Subparser):
     """
     Create a new virtualenv
     """
@@ -81,11 +93,56 @@ class Create(Subparser):
     def add_arguments(self):
         self.subparser.add_argument(
             'venv', help="Path to a virtualenv")
-        self.subparser.add_argument('--src', default='src',
-                               help="Directory for downloaded source code")
+        self.subparser.add_argument(
+            '--src', default='src', help="Directory for downloaded source code")
 
     def action(self, args):
         create_virtualenv(args.venv, srcdir=args.src)
+
+
+class Wheel(Subparser):
+    """
+    Create the WHEELHOUSE, optionally building wheels
+    """
+
+    def add_arguments(self):
+        self.subparser.add_argument(
+            '-w', '--wheelstreet', default=WHEELSTREET_USER,
+            help="""Directory for wheels and virtualenv [%(default)s]""")
+        self.subparser.add_argument(
+            '-r', '--requirements',
+            help="""file containing list of packages to install"""
+        )
+
+    def action(self, args):
+        wheelhouse = path.join(args.wheelstreet, PY_VERSION)
+        src = path.join(wheelhouse, 'src')
+        cache = path.join(wheelhouse, 'cache')
+        venv = path.join(wheelhouse, 'venv')
+        create_virtualenv(venv, src)
+
+        pip = path.join(venv, 'bin', 'pip')
+
+        subprocess.check_call(
+            [pip, 'install', '--download-cache', cache, WHEEL_PKG])
+
+        if args.requirements:
+            with open(args.requirements) as f:
+                for pkg in f:
+                    subprocess.check_call([
+                        pip, 'wheel', pkg,
+                        '--download-cache', cache,
+                        '--use-wheel',
+                        '--find-links', wheelhouse,
+                        '--wheel-dir', wheelhouse
+                    ])
+
+                    subprocess.check_call([
+                        pip, 'install', pkg,
+                        '--use-wheel',
+                        '--no-index',
+                        '--find-links', wheelhouse
+                    ])
 
 
 def main(arguments):
@@ -95,12 +152,12 @@ def main(arguments):
         formatter_class=argparse.RawDescriptionHelpFormatter)
 
     subparsers = parser.add_subparsers()
-    create = Create(subparsers, name='create')
+    Virtualenv(subparsers, name='virtualenv')
+    Wheel(subparsers, name='wheel')
 
     args = parser.parse_args(arguments)
+    print('using {} ({})'.format(sys.executable, PY_VERSION))
     args.func(args)
-
-    # py_version = "{}.{}.{}".format(*sys.version_info[:3])
 
 
 if __name__ == '__main__':
