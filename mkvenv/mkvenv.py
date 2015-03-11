4e25b0e4be68cca26df4134913fd277c9672e487
Noah Hoffman <nhoffman@positron.local>
2015-03-10 20:53:59 -0700

    further normalize version numbering

--- mkvenv/mkvenv.py
+++ mkvenv/mkvenv.py
@@ -59,7 +59,7 @@ from distutils.version import LooseVersion
 
 try:
     with open(path.join(path.dirname(__file__), 'data', 'ver')) as f:
-        __version__ = f.read().strip().replace('-', '+', 1)
+        __version__ = f.read().strip().replace('-', '+', 1).replace('-', '.')
 except Exception, e:
     __version__ = ''
 
